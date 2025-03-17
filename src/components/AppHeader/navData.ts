fdf9fe717453278eac77687e04c1141ab0587240
kacper-sx <kacper.saweczko@o2.pl>
2025-03-17 13:53:30 +0100

    initial commit

---
+++ src/components/AppHeader/navData.ts
@@ -0,0 +1,8 @@
+
+
+export const navData = [
+  {href: "/akutalnosci", title: "Aktualności"},
+  {href: "/muzycy", title: "Muzycy"},
+  {href: "/historia", title: "Historia"},
+  {href: "/kontakt", title: "Kontakt"},
+]
