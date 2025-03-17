0bbc950f7e3aac00a4aca71af73b76c69655fdd1
kacper-sx <kacper.saweczko@o2.pl>
2025-03-17 18:18:41 +0100

    feat finish landing

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,37 @@
 import { defineConfig } from 'astro/config';
 
 import react from '@astrojs/react';
-
+import icon from "astro-icon";
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  site: 'https://awm-awf.pl',
+  integrations: [icon({
+      include: {
+        lucide: [
+          "arrow-up-right",
+          "chevron-right",
+          "phone",
+          "menu",
+          "scroll",
+          "mail",
+          "guitar",
+          "crown",
+          "handshake",
+          "mic",
+          "map-pin",
+          "calendar-clock",
+          "hand-coins",
+          "calendar",
+          "heater",
+          "server",
+          'air-vent',
+          "battery-charging",
+          "cctv"
+        ],
+      },
+    }), react()],
 
   vite: {
     plugins: [tailwindcss()]
