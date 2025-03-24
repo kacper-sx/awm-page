ddf21df3c522f08130fdfcc2cd097e08b92b8ae6
kacper-sx <kacper.saweczko@o2.pl>
2025-03-24 23:05:41 +0100

    fix: update utils

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -11,3 +11,9 @@ export const qsAll = <T extends string>(selector: T) => {
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export const dqsAll = (selector: string) =>
+  Array.from(document.querySelectorAll(selector)).filter(isHTMLElement);
+
+export const isHTMLElement = (el: unknown): el is HTMLElement =>
+  el instanceof HTMLElement;
