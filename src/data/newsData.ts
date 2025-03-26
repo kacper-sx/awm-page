86b6f8581d79fec934a4dff676a6f546a14796b0
kacper-sx <kacper.saweczko@o2.pl>
2025-03-26 12:44:26 +0100

    refactor: change news

---
+++ src/data/newsData.ts
@@ -0,0 +1,26 @@
+import P1 from "../assets/band/Igor.avif";
+
+
+
+
+export const newsData = [
+  {
+    title: "Igor Krupa",
+    image: P1,
+    date: "2023-08-01",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quae.",
+    author: "Igor Krupa",
+    link: "/aktualnosci/dzien-kobiet",
+    alt: "dzien kobiet"
+  },
+  {
+    title: "Kacper",
+    image: P1,
+    date: "2023-08-01",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quae.",
+    author: "Igor Krupa",
+    link: "/aktualnosci/dzien-kobiet",
+    alt: "dzien kobiet"
+  },
+  
+];
