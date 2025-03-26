0126cd12abf975da81e57def42174284d3ff6ea8
kacper-sx <kacper.saweczko@o2.pl>
2025-03-26 14:07:14 +0100

    feat: add news positions and init shadcn

--- src/data/newsData.ts
+++ src/data/newsData.ts
@@ -1,26 +1,27 @@
-import P1 from "../assets/band/Igor.avif";
+import P2 from "../assets/galeria/dzien-kobiet.jpg";
+import P1 from "../assets/galeria/magia-swiat.jpg";
 
 
 
 
 export const newsData = [
   {
-    title: "Igor Krupa",
-    image: P1,
-    date: "2023-08-01",
-    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quae.",
+    title: "Dzień Kobiet 2025",
+    image: P2,
+    date: "2024-12-11",
+    description: "Uroczysty koncert z okazji dnia kobiet z udziałem władz uczelni.",
     author: "Igor Krupa",
     link: "/aktualnosci/dzien-kobiet",
     alt: "dzien kobiet"
   },
   {
-    title: "Kacper",
+    title: "Magia Świąt 2025",
     image: P1,
-    date: "2023-08-01",
-    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quae.",
+    date: "2025-03-05",
+    description: "Koncert piosenek świątecznych i muzyki rozrywkowej.",
     author: "Igor Krupa",
-    link: "/aktualnosci/dzien-kobiet",
-    alt: "dzien kobiet"
+    link: "/aktualnosci/magia-swiat",
+    alt: "magia swiat"
   },
   
 ];
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,16 +1,14 @@
-import { type ClassValue, clsx } from "clsx";
-import { twMerge } from "tailwind-merge";
+import { clsx, type ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
 
-
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
 
 export const qsAll = <T extends string>(selector: T) => {
   const els = document.querySelectorAll(selector)
   return Array.from(els).filter(el => el instanceof HTMLElement)
 } 
-
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
-}
 
 export const dqsAll = (selector: string) =>
   Array.from(document.querySelectorAll(selector)).filter(isHTMLElement);
